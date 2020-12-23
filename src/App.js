1cbd8d745acd1773396c9e6517db5f400aae18f9
Roger Fan <rogerjoeyfan@gmail.com>
2020-12-23 12:31:47 -0800

    Redoing everything, will save that for later

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import './css/app.css';
 import React from 'react';
-import TopSketch from './components/topsketch'
-// import TestSketch from './components/testsketch'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      brr: 1
+
     }
   }
 
   render() {
-    return <div onClick={(e) => { this.setState({ brr: this.state.brr + 1 }) }}>
-      <div id="canvas-container">
-        <TopSketch />
-      </div>
-      <div id="start"></div>
-      <div id="content">
-        Test brr haha <br />
-        Brrrr <br />
-        Lorem ipsum <br />
-      </div>
+    return <div>
+      
     </div>
   }
 }
